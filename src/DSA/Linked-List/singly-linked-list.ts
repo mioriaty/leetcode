cb27e619e43905c4e1a499e1adf92cf51d98d439
duongto <mioriaty@gmail.com>
2024-01-02 21:22:32 +0700

    adding

--- src/DSA/Linked-List/singly-linked-list.ts
+++ src/DSA/Linked-List/singly-linked-list.ts
@@ -9,47 +9,114 @@ class ListNode<T> {
 }
 
 class SinglyLinkedList<T> {
-  head: ListNode<T> | null;
-  tail: ListNode<T> | null;
-  length: number;
+  constructor() {}
 
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  printLinkedList(): void {
-    let current = this.head;
+  printLinkedList(head: ListNode<T> | null): void {
+    let current = head;
     if (!current) {
       console.log("Empty List");
     } else {
       while (current) {
-        console.log(current.value);
+        console.log(`${current.value} ->`);
         current = current.next;
 
         if (!current) {
           console.log("null");
-        } else {
-          console.log("->");
         }
       }
     }
   }
+
+  // Thêm phần tử vào đầu danh sách
+  addToHead(value: T, headNode: ListNode<T> | null): ListNode<T> {
+    const newNode = new ListNode(value);
+
+    if (headNode !== null) {
+      newNode.next = headNode;
+      return newNode;
+    }
+
+    return newNode;
+  }
+
+  // Thêm phần tử vào cuối danh sách
+  addToTail(value: T, headNode: ListNode<T> | null): ListNode<T> {
+    const newNode = new ListNode(value);
+
+    if (headNode === null) {
+      return newNode;
+    } else {
+      // Xác định last: phần tử cuối cùng luôn trở vào NULL;
+      let lastNode = headNode;
+      while (lastNode.next !== null) {
+        lastNode = lastNode.next;
+      }
+      // Khi đã tìm thấy last node thì gán next của last node bằng newNode
+      lastNode.next = newNode;
+    }
+
+    // Trả về headNode, vì headNode vẫn là phần tử đầu tiên của danh sách
+    return headNode;
+  }
+
+  // Thêm phần tử vào 1 index chỉ định
+  addToIndex(
+    value: T,
+    index: number,
+    headNode: ListNode<T> | null
+  ): ListNode<T> {
+    const newNode = new ListNode(value);
+
+    if (headNode === null) {
+      return newNode;
+    }
+
+    if (index === 0) {
+      return this.addToHead(value, headNode);
+    } else {
+      // Tìm vị trí cần thêm
+      let current = headNode;
+      let count = 0;
+
+      while (current.next !== null) {
+        count++;
+        if (count === index) {
+          // Thực hiện add vào
+          newNode.next = current.next;
+          current.next = newNode;
+          break;
+        }
+        current = current.next;
+      }
+    }
+
+    return headNode;
+  }
+
+  // call main function
+  main() {
+    const list = new SinglyLinkedList<number>();
+
+    let n1 = new ListNode(1);
+    const n2 = new ListNode(2);
+    const n3 = new ListNode(3);
+
+    n1.next = n2;
+    n2.next = n3;
+
+    // #Add to head
+    n1 = list.addToHead(0, n1);
+
+    // #Add to tail
+    n1 = list.addToTail(4, n1);
+
+    // #Add to index
+    n1 = list.addToIndex(5, 3, n1);
+
+    // print list
+    list.printLinkedList(n1);
+  }
 }
 
 // create example list
-const list = new SinglyLinkedList<number>();
-
-const n1 = new ListNode(1);
-const n2 = new ListNode(2);
-const n3 = new ListNode(3);
-
-n1.next = n2;
-n2.next = n3;
-
-list.head = n1;
-list.tail = n3;
-list.length = 3;
-
-list.printLinkedList();
+new SinglyLinkedList<number>().main();
